9b8271a747f533959a1a36a306161cb1cc46c582
SED4906 <sed4906birdie@gmail.com>
2025-06-10 16:48:39 -0700

    installed unit links

--- src/unit/activation.rs
+++ src/unit/activation.rs
@@ -6,12 +6,21 @@ const UNIT_PATHS: &[&str] = &["/etc/systemd/system/","/usr/lib/systemd/system/"]
 
 pub fn walk(node: String) -> Vec<Unit> {
     let mut queue = vec![];
-    if let Some(name) = check_path(node) {
+    if let Some(name) = check_path(node.clone()) {
         if let Ok(info) = name.info() {
             let requires = info.depend("Requires");
-            let wants = info.depend("Wants");
+            let mut wants = info.depend("Wants");
             let mut after = info.depend("After");
             let before = info.depend("Before");
+            match info.unit_type {
+                UnitType::Target => {
+                    let mut subdir = String::new();
+                    subdir.push_str(node.as_str());
+                    subdir.push_str(".wants");
+                    wants.append(&mut check_path_dir(subdir));
+                }
+                _ => {}
+            }
             for subnode in requires.clone().into_iter() {
                 queue.append(&mut walk(subnode));
             }
@@ -82,6 +91,28 @@ fn check_path(node: String) -> Option<UnitName> {
         return Some(UnitName { unit_file, name, template });
     }
     None
+}
+
+fn check_path_dir(subdir: String) -> Vec<String> {
+    let mut units = vec![];
+    for unit_path in UNIT_PATHS {
+        let mut unit_subdir: PathBuf = unit_path.into();
+        unit_subdir.push(subdir.clone());
+        if std::fs::exists(&unit_subdir).ok().is_none_or(|p| !p) {
+            continue;
+        }
+        if let Ok(read_dir) = std::fs::read_dir(&unit_subdir) {
+            for unit_link in read_dir {
+                if let Ok(unit_link) = unit_link {
+                    if let Ok(node) = unit_link.file_name().into_string() {
+                        units.push(node);
+                    }
+                }
+            }
+        }
+
+    }
+    units
 }
 
 impl UnitInfo {
